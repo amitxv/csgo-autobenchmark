b0dc2aade34180eb102579bf48d07c987d7aa999
AMIT <86924251+amitxv@users.noreply.github.com>
2022-07-21 15:20:46 +0100

    enable delayed expansion

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 :: Requirements
 ::
@@ -6,55 +7,55 @@
 :: - 7-Zip
 
 set "path_err=0"
-for %%i in (
+for %%a in (
     "python.exe",
     "pip.exe",
     "7z.exe"
 ) do (
-    where %%i
-    if not %errorlevel% == 0 (
+    where %%a
+    if not !errorlevel! == 0 (
         set "path_err=1"
-        echo error: %%i not found in path
+        echo error: %%a not found in path
     )
 )
-if not %path_err% == 0 exit /b
+if not !path_err! == 0 exit /b
 
 set "CURRENT_DIR=%~dp0"
-set "CURRENT_DIR=%CURRENT_DIR:~0,-1%"
+set "CURRENT_DIR=!CURRENT_DIR:~0,-1!"
 
-set "BUILD_ENV=%CURRENT_DIR%\BUILD_ENV"
-set "PROJECT_DIR=%BUILD_ENV%\main"
-set "PUBLISH_DIR=%BUILD_ENV%\csgo-autobenchmark"
+set "BUILD_ENV=!CURRENT_DIR!\BUILD_ENV"
+set "PROJECT_DIR=!BUILD_ENV!\main"
+set "PUBLISH_DIR=!BUILD_ENV!\csgo-autobenchmark"
 
-if exist "%BUILD_ENV%" (
-    rd /s /q "%BUILD_ENV%"
+if exist "!BUILD_ENV!" (
+    rd /s /q "!BUILD_ENV!"
 )
-mkdir "%BUILD_ENV%"
-mkdir "%PROJECT_DIR%"
+mkdir "!BUILD_ENV!"
+mkdir "!PROJECT_DIR!"
 
-python -m venv "%BUILD_ENV%"
-call "%BUILD_ENV%\Scripts\activate.bat"
+python -m venv "!BUILD_ENV!"
+call "!BUILD_ENV!\Scripts\activate.bat"
 
 pip install -r requirements.txt
 
-copy /y "%CURRENT_DIR%\src\csgo-autobenchmark.py" "%PROJECT_DIR%"
-cd "%PROJECT_DIR%"
+copy /y "!CURRENT_DIR!\src\csgo-autobenchmark.py" "!PROJECT_DIR!"
+cd "!PROJECT_DIR!"
 
 pyinstaller "csgo-autobenchmark.py" --onefile --uac-admin
 
-call "%BUILD_ENV%\Scripts\deactivate.bat"
+call "!BUILD_ENV!\Scripts\deactivate.bat"
 
-cd "%CURRENT_DIR%"
+cd "!CURRENT_DIR!"
 
-xcopy /s /i /e "%CURRENT_DIR%\src" "%PUBLISH_DIR%"
-del /f /q "%PUBLISH_DIR%\csgo-autobenchmark.py"
-move "%PROJECT_DIR%\dist\csgo-autobenchmark.exe" "%PUBLISH_DIR%"
+xcopy /s /i /e "!CURRENT_DIR!\src" "!PUBLISH_DIR!"
+del /f /q "!PUBLISH_DIR!\csgo-autobenchmark.py"
+move "!PROJECT_DIR!\dist\csgo-autobenchmark.exe" "!PUBLISH_DIR!"
 
 if exist "csgo-autobenchmark.zip" (
     del /f /q "csgo-autobenchmark.zip"
 )
-7z a -tzip "csgo-autobenchmark.zip" "%PUBLISH_DIR%"
+7z a -tzip "csgo-autobenchmark.zip" "!PUBLISH_DIR!"
 
-rd /s /q "%BUILD_ENV%"
+rd /s /q "!BUILD_ENV!"
 
 exit /b
