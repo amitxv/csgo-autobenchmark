27737f8f84bc7d15079d450e9d10bc0df91f3c78
AMIT <86924251+amitxv@users.noreply.github.com>
2022-10-12 13:30:36 +0100

    Update version

--- src/csgo-autobenchmark.py
+++ src/csgo-autobenchmark.py
@@ -80,7 +80,7 @@ def timer_resolution(enabled: bool) -> int:
 def main() -> int:
     """cli entrypoint"""
 
-    version = "0.3.2"
+    version = "0.3.3"
     subprocess_null = {"stdout": subprocess.DEVNULL, "stderr": subprocess.DEVNULL}
     cfg = parse_config("config.txt")
     present_mon = "PresentMon-1.6.0-x64.exe"
