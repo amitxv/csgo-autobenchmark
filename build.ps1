86e44cd64386186e51b450159024e986787dbde4
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-06 10:34:58 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,17 +1,19 @@
 function main() {
     # pack executable
-    pyinstaller src\csgo-autobenchmark.py --onefile
+    pyinstaller ".\src\csgo-autobenchmark.py" --onefile
 
-    if (Test-Path "csgo-autobenchmark") {
-        Remove-Item -Path "csgo-autobenchmark" -Recurse
+    if (Test-Path ".\csgo-autobenchmark\") {
+        Remove-Item -Path ".\csgo-autobenchmark\" -Recurse
     }
 
     # create folder structure
-    New-Item -ItemType Directory -Path csgo-autobenchmark
-    Move-Item dist\csgo-autobenchmark.exe csgo-autobenchmark
-    Move-Item src\bin csgo-autobenchmark
-    Move-Item src\prerequisites csgo-autobenchmark
-    Move-Item src\config.txt csgo-autobenchmark
+    New-Item -ItemType Directory -Path ".\csgo-autobenchmark\"
+
+    # create final package
+    Move-Item ".\dist\csgo-autobenchmark.exe" ".\csgo-autobenchmark\"
+    Move-Item ".\src\bin\" ".\csgo-autobenchmark\"
+    Move-Item ".\src\prerequisites\" ".\csgo-autobenchmark\"
+    Move-Item ".\src\config.txt\" ".\csgo-autobenchmark\"
 
     return 0
 }
