05704045efecbed05ee1243ae0d2ded00bbc7c53
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-08 18:47:44 +0000

    update files to move in build script

--- build.ps1
+++ build.ps1
@@ -30,8 +30,8 @@ function main() {
     # create final package
     Copy-Item ".\build\pyinstaller\dist\csgo-autobenchmark.exe" ".\build\csgo-autobenchmark\"
     Copy-Item ".\csgo_autobenchmark\bin\" ".\build\csgo-autobenchmark\" -Recurse
-    Copy-Item ".\csgo_autobenchmark\prerequisites\" ".\build\csgo-autobenchmark\" -Recurse
     Copy-Item ".\csgo_autobenchmark\config.txt" ".\build\csgo-autobenchmark\"
+    Copy-Item ".\csgo_autobenchmark\video.txt" ".\build\csgo-autobenchmark\"
 
     return 0
 }
